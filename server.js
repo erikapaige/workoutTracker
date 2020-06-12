bc524ae02fac64dbef20e6b79b26df9e570eb6eb
Erika <erika.paige.handley@gmail.com>
2020-06-11 21:04:58 -0700

    added models and routes folders

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+// bring in the express package
 const express = require('express')
+// bring in package morgan
+const morgan = require('morgan')
 const app = express()
 
+// static middlewear
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // app.use(require('./routes'))
 
+// bring in the config folder that has the connection to mongoose
 require('./config')
   .then(() => app.listen(3000))
   .catch(err => console.error(err))
