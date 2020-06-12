3043cf17feef1aed37f27f07d700bf18f51e0cf7
Erika <erika.paige.handley@gmail.com>
2020-06-11 21:41:56 -0700

    basic CRUD routes

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -3,5 +3,41 @@ const router = require('express').Router()
 // bring in the workout model
 const { Workout } = require('../models')
 
+// GET all workouts
+router.get('/workout', (req, res) => {
+  Workout.find()
+    .then(workout => res.json(workout))
+    .catch(err => console.error(err))
+})
+
+// GET one workout
+router.get('/workout/:id', (req, res) => {
+  Workout.findById(req.params.id)
+    .then(workout => res.json(workout))
+    .catch(err => console.error(err))
+})
+
+// POST (ADD) one workout
+router.post('/workout', (req, res) => {
+  Workout.create(req.body)
+    .then(workout => res.json(workout))
+    .catch(err => console.error(err))
+})
+
+// PUT (UPDATE) one workout
+router.put('/workout/:id', (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => res.sendStatus(200))
+    .catch(err => console.error(err))
+})
+
+// DELETE one workout
+router.delete('/workout/:id', (req, res) => {
+  Workout.findByIdAndDelete(req.params.id)
+    .then(() => res.sendStatus(200))
+    .catch(err => console.error(err))
+})
+
+
 // export router
 module.exports = router
