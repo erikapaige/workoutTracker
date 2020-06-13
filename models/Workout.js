88d02341a002a8cafac055b7e72cd5c43a74e68d
Erika <erika.paige.handley@gmail.com>
2020-06-12 18:10:24 -0700

    added requrired to schema

--- models/Workout.js
+++ models/Workout.js
@@ -1,47 +1,57 @@
 // bring in mongoose
 const { Schema, model } = require('mongoose')
 
-//building Schema 
+// building Schema 
+// think about virtual properties, items do not want user to see, that would help gain total value
+// 
 const workoutSchema = new Schema({
   {
+    // newDate(), needs date of workout
     day: {
-      // to get date of workout
       type: Date,
       // need to set new date
     },
+    // all exercises belong to workout, so add via an array of objects to be stored 
     exercises: [{
+      // specify data keys (properties) allowed on exercise
         type: 
           {
             type: String, 
-            //assign other properties 
+            required: true
             // assign prompt for use
           },
         name:
           {
             name: String, 
+            required: true
             //assign other properties
             // ask user for name?
           }, 
         duration: 
           {
-            type: Number
+            type: Number,
+            required: true
             // ask user for number?
           },
         weight:
           {
-            type: Number
+            type: Number,
+            required: true
           },
         distance:
           {
-            type: Number
-          },
-        sets:
-          {
-            type: Number
+            type: Number,
+            required: true
           },
         reps:
           {
-            type: Number
+            type: Number,
+            required: true
+          },
+        sets:
+          {
+            type: Number,
+          required: true
           }
       }]
   }
